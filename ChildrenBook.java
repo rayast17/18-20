d1a51883ee7421e0023438039b984fb2ba13b664
rayast17 <72138164+rayast17@users.noreply.github.com>
2020-12-14 20:01:10 +0200

    Add files via upload

---
+++ ChildrenBook.java
@@ -0,0 +1,23 @@
+package myonlineshop;
+
+public class ChildrenBook extends Book {
+	private int age;
+
+	public ChildrenBook(double regularPrice, String publisher, int yearPublished, int age) {
+		super(regularPrice, publisher, yearPublished);
+		setAge(age);
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	// Override this method
+		public double computeSalePrice() {
+			return super.getRegularPrice() * 0.3;
+		}
+}
